50acf4c49f74e0248cbe3357beae0c4f09c41af1
Hanane <h@Hananes-MacBook-Air.local>
2026-04-01 23:54:32 -0400

    Edited file in parallel branch

--- 01-read_input.sh
+++ 01-read_input.sh
@@ -10,3 +10,11 @@ echo 'Your name was stored in $name'
 
 # exercise: write a script that asks the user for a 
 # filename and create an empty file named after it
+echo "Enter a filename:"
+read filename
+
+touch $filename
+
+echo "File $filename created"
+
+echo "This is a different change in parallel branch"
